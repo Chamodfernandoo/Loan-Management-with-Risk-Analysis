0192e9e4908de8d292bd7c5d53e3e09792a7ee89
Chamodfernandoo <chamoddilshan0907@gmail.com>
2025-01-25 19:04:52 +0530

    data table

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, } from "react-router-dom";
 import Lender_dashbord from "./pages/Lender/lender_dashbord";
 import Customer_dashbord from "./pages/Customer/customer_dashbord";
+import Viwe_loans from "./pages/Lender/viwe_loans";
 
 const App: React.FC = () => {
   return (
@@ -10,8 +11,8 @@ const App: React.FC = () => {
       {/* lender routes */}
       <Routes>
         <Route path="/" element={<Lender_dashbord/>} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> */}
+         <Route path="/view_loan" element={<Viwe_loans />} />
+        {/*<Route path="/contact" element={<Contact />} /> */}
       </Routes>
 
     {/* customer routes */}
