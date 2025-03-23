003c644ae9ddbe4658c549c718ed2f0db32302a5
Chamodfernandoo <chamoddilshan0907@gmail.com>
2025-03-23 20:47:37 +0530

    edit lender dashbord and oder history page(add agreemnt button)

--- Frontend/src/pages/Lender/All-loans/column.tsx
+++ Frontend/src/pages/Lender/All-loans/column.tsx
@@ -129,17 +129,18 @@ export const columns: ColumnDef<Loan>[] = [
       return (
         <div className="flex items-center gap-2">
           <Button variant="outline" size="sm" asChild>
-            <Link to={`/customers/${loan.id}`}>
-              <Eye className="mr-2 h-4 w-4" />
-              View Customer
-            </Link>
-          </Button>
-          <Button variant="outline" size="sm" asChild>
             <Link to={`/invoice`}>
               <FileText className="mr-2 h-4 w-4" />
               View Invoice
             </Link>
           </Button>
+          <Button variant="outline" size="sm" asChild>
+            <Link to={`/customers/${loan.id}`}>
+              <Eye className="mr-2 h-4 w-4" />
+              Agreement
+            </Link>
+          </Button>
+          
         </div>
       )
     },
